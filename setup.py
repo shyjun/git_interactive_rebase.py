30876c7fda301164c71131f6755f714e8eb3827a
shyjun <snarangaprath@innophaseiot.com>
2026-03-20 15:26:07 +0530

    package version with pip package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import subprocess
+import json
+from datetime import datetime
+from pathlib import Path
+from setuptools import setup
+
+
+def get_git_sha():
+    try:
+        return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
+    except Exception:
+        return "unknown"
+
+
+data = {
+    "sha": get_git_sha(),
+    "date": datetime.utcnow().strftime("%Y-%m-%d"),
+    "repo": "https://github.com/shyjun/git-interactive-rebase-gui-tool",
+}
+
+Path("app_version.json").write_text(json.dumps(data, indent=2))
+
+setup()
